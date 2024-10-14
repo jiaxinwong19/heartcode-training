debc997b3e496a7a29fc96dbca9135a130ad4619
Wong Jia XIn <jiaxin.wong.2023@scis.smu.edu.sg>
2024-10-14 17:06:26 +0800

    Update page.tsx
    o8yiutrhdgrfg

--- my-app/src/app/home/page.tsx
+++ my-app/src/app/home/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
                 <h1 className="font-bold text-5xl">Taking drugs is not
                     <FlipWords words={words} />
                 </h1>
+                <div>oiuyktjrhgsefargdtfyguhijlikuyjfthdgrdf</div>
             </div>
         </div>
     )
